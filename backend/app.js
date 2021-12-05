15dc653b76262ddd61503beb0810accbaa5546c1
Mahiet Bruno <devwebmb@gmail.com>
2021-12-05 17:23:41 +0100

    signup ok

---
+++ backend/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/user");
+
+const app = express();
+
+// connection base de données
+
+mongoose
+  .connect(
+    "mongodb+srv://nono7779:nono7779@cluster0.d9ehs.mongodb.net/piiquante?retryWrites=true&w=majority",
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+//gestion du cors
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+
+app.use("/api/auth", userRoutes);
+
+module.exports = app;
